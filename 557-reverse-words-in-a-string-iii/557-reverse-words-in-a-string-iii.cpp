c9670b8ab17945157827cfb4aff330a847eb6ee0
kakkarot0 <94862104+kakkarot0@users.noreply.github.com>
2022-09-24 19:37:52 +0530

    Time: 51 ms (23.06%), Space: 9.7 MB (78.37%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -3,6 +3,7 @@ public:
     string reverseWords(string s) {
         int last=-1;
         for(int i=0;i<=s.length();++i){
+            // <= we considerd this because of the end=i-1 so it give last end of string
           if(i==s.length()||s[i]==' '){
              int start=last+1;
               int end=i-1;
