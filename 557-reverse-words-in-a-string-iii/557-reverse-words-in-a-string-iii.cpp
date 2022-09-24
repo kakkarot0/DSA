82442b4924acae5be5f5e45f3ca720aa51631ef5
kakkarot0 <94862104+kakkarot0@users.noreply.github.com>
2022-09-24 19:17:26 +0530

    Time: 16 ms (85.48%), Space: 9.6 MB (78.37%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string result;
-        int last=-1;
-        for(int i=0;i<=s.length();++i){
-           if(s[i]==' '||(i==s.length()-1)){
-              int rev=
-                  (i==s.length()-1)?i:i-1;
-               for(;rev>last;rev--){
-                   result+=s[rev];
-               }
-               if(i!=s.length()-1){
-                   result+=' ';
-               }
-               last=i;
-           }
+         int lastSpaceIndex = -1;
+        int len = (int)s.size();
+        for (int strIndex = 0; strIndex <= len; strIndex++) {
+            if (strIndex == len || s[strIndex] == ' ') {
+                int startIndex = lastSpaceIndex + 1;
+                int endIndex = strIndex - 1;
+                while (startIndex < endIndex) {
+                    char temp = s[startIndex];
+                    s[startIndex] = s[endIndex];
+                    s[endIndex] = temp;
+                    startIndex++;
+                    endIndex--;
+                }
+                lastSpaceIndex = strIndex;
+            }
         }
-        return result;
+        return s;
     }
 };
