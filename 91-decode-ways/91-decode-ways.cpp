db21ddde8dc3c182d0c037fba9f90f40330de4d2
kakkarot0 <94862104+kakkarot0@users.noreply.github.com>
2022-10-02 00:49:04 +0530

    Time: 14 ms (6.59%), Space: 15.9 MB (5.07%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,5 +1,6 @@
 class Solution {
-public:
+public: 
+    //tjis is top down approach
     vector<int>dp;
     int numDecodings(string s) {
         dp.assign(s.size(),-1);
@@ -7,15 +8,17 @@ public:
     }
     int nways(int i,string s){
       if(i>=s.size())return 1;
-        else if(s[i]=='0') return 0;
+        else if(s[i]=='0') return 0;   //case consider "0"
       else if(i==s.size()-1)return 1;
         else if (dp[i] != -1)
-            return dp[i];
-      
+            return dp[i];   //memo
+      //        0-9                  0-6
         else if(s[i]=='1'||(s[i]=='2' && (s[i+1]>='0'&&s[i+1]<='6'))){
             return dp[i] = nways(i+1,s)+nways(i+2,s);
-        }
+            // this shows that there are two ways or two choices
+        }  
         else return dp[i] = nways(i+1,s);
+        // if first indes is greater than 2 
          
     
     }
