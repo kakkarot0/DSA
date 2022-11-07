e523ec79c6ac4264a026705a8dace07a79a7788c
kakkarot0 <94862104+kakkarot0@users.noreply.github.com>
2022-11-07 10:38:52 +0530

    Time: 3 ms (38.22%), Space: 6.2 MB (8.65%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-          string s_num=to_string(num);
-        //converted to string
-          for(auto &c : s_num){
-              if(c=='6'){
-                  c='9';
-                  break;
-                  //for the maximum we need to change the firt digit to 9
-              }
-              
-          }
-        return stoi(s_num);
+        int numcopy=num;
+        int countdigit=0;
+        int firstindexsix=-1;
+        while(numcopy>0){
+            if(numcopy%10==6){
+                firstindexsix=countdigit;
+            }
+            numcopy=numcopy/10;
+            countdigit++;
+        }
+        return (firstindexsix==-1?num:num+3*pow(10,firstindexsix));
     }
 };
