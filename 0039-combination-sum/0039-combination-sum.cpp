2b69929565963c89251e0866f1ed6a28e5144328
kakkarot0 <94862104+kakkarot0@users.noreply.github.com>
2022-11-12 18:47:24 +0530

    Time: 3 ms (97.28%), Space: 10.7 MB (92.83%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     void sum(int ind,int target,vector<int>&arr,vector<vector<int>>&ans,vector<int>&ds){
-     if(ind==arr.size()){
-         if(target==0){
-            ans.push_back(ds);
-         }
-         return;
-     }
-        //if we decide to pick up the element we need to check
+        if(ind==arr.size()){
+           if(target==0){
+               ans.push_back(ds);
+           }
+            return ;
+        }
+       
         if(arr[ind]<=target){
-          ds.push_back(arr[ind]);
-          sum(ind,target-arr[ind],arr,ans,ds);
-          ds.pop_back(); //when u comeback make sure to pop this element
+           ds.push_back(arr[ind]);
+       sum(ind,target-arr[ind],arr,ans,ds);
+           ds.pop_back();
         }
         sum(ind+1,target,arr,ans,ds);
     
-    }
-    
-    
-    
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
         vector<int>ds;
         sum(0,target,candidates,ans,ds);
-       
         return ans;
     }
 };
