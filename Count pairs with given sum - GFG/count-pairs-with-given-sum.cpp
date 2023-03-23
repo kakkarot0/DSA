a16f76048c4ee3b5b61ddfe362a59309a3803a1f
Arun Gound <94862104+kakkarot0@users.noreply.github.com>
2023-03-23 16:57:30 +0530

    Added solution - LeetHub

--- Count pairs with given sum - GFG/count-pairs-with-given-sum.cpp
+++ Count pairs with given sum - GFG/count-pairs-with-given-sum.cpp
@@ -12,15 +12,15 @@ public:
     int getPairsCount(int arr[], int n, int k) {
         // code here
         unordered_map<int, int> m;
-    int count = 0;
-    for (int i = 0; i < n; i++) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
         if (m.find(k - arr[i]) != m.end()) {
             count += m[k - arr[i]];
         }
         m[arr[i]]++;
-    }
-    return count;
-    }
+        }
+        return count;
+        }
 };
 
 //{ Driver Code Starts.
