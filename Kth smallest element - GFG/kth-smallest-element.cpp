38421dbfb120152897244c76a9b7f09876e5ae1b
Arun Gound <94862104+kakkarot0@users.noreply.github.com>
2023-03-27 15:28:49 +0530

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.cpp
+++ Kth smallest element - GFG/kth-smallest-element.cpp
@@ -15,13 +15,10 @@ class Solution{
     // k : find kth smallest element and return using this function
     int kthSmallest(int arr[], int l, int r, int k) {
         //code here
-        
         priority_queue<int>maxh;
-        for(int i=0;i<r+1;++i){
+        for(int i=0;i<r+1;i++){
             maxh.push(arr[i]);
-            if(maxh.size()>k){
-                maxh.pop();
-            }
+            if(maxh.size()>k)maxh.pop();
         }
         return maxh.top();
     }
