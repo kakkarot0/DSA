eede07aadf6ace63a0ea17b5eb97d383669a2223
Arun Gound <94862104+kakkarot0@users.noreply.github.com>
2023-04-14 13:09:05 +0530

    Added solution - LeetHub

--- Remove the balls - GFG/remove-the-balls.cpp
+++ Remove the balls - GFG/remove-the-balls.cpp
@@ -7,21 +7,19 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
+// User function Template for C++
+
 class Solution {
   public:
     int finLength(int N, vector<int> color, vector<int> radius) {
         stack<pair<int,int>>st;
         for(int i=0;i<N;i++){
-            if(st.empty()){
-            st.push({color[i],radius[i]});
-            }
             
-        else if(st.top().first==color[i]&&st.top().second==radius[i]){
+         if(!st.empty()&&(st.top().first==color[i]&&st.top().second==radius[i])){
             st.pop();
         }
-        else {
-            st.push({color[i],radius[i]});
-        }
+        else st.push({color[i],radius[i]});
+        
        }
         return st.size();
     }
